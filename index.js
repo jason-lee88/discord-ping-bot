5c5d32efac75bbe5e4b7b2f73575736308659b3c
Jason Lee <jason.e.lee8@gmail.com>
2020-07-14 18:45:30 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require('./config.json');
+
+var prefix = "!"
+var channelName = "ping-bot"
+
+client.once("ready", () => {
+	console.log(client.user.username + " is online!");
+});
+
+client.on("message", message => {
+    if (message.content == prefix + "ping" && message.channel.name == channelName) {
+        message.channel.send("Pong!").then(response => {
+            var ping = response.createdAt - message.createdAt;
+            message.channel.send(ping + " ms")
+        });
+    }
+});
+
+client.login(config.TOKEN);
